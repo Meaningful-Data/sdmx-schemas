cdbe3b180f9962d2189d1e7b6a6f400982ef6156
javier.hernandez <javier.hernandez@meaningfuldata.eu>
2024-05-26 10:29:40 +0200

    Initial commit.

---
+++ tests/test_reading_schemas.py
@@ -0,0 +1,31 @@
+from sdmxschemas import SDMX_JSON_10_DATA_PATH, SDMX_JSON_10_METADATA_PATH, SDMX_JSON_10_STRUCTURE_PATH, SDMX_JSON_20_DATA_PATH, \
+    SDMX_JSON_20_METADATA_PATH, SDMX_JSON_20_STRUCTURE_PATH, SDMX_ML_10_MESSAGE_PATH, SDMX_ML_20_MESSAGE_PATH, SDMX_ML_21_MESSAGE_PATH, \
+    SDMX_ML_30_MESSAGE_PATH
+
+
+def test_reading_sdmx_json_10():
+    assert SDMX_JSON_10_DATA_PATH.exists()
+    assert SDMX_JSON_10_STRUCTURE_PATH.exists()
+    assert SDMX_JSON_10_METADATA_PATH.exists()
+
+
+def test_reading_sdmx_json_20():
+    assert SDMX_JSON_20_DATA_PATH.exists()
+    assert SDMX_JSON_20_STRUCTURE_PATH.exists()
+    assert SDMX_JSON_20_METADATA_PATH.exists()
+
+
+def test_reading_sdmx_ml_10():
+    assert SDMX_ML_10_MESSAGE_PATH.exists()
+
+
+def test_reading_sdmx_ml_20():
+    assert SDMX_ML_20_MESSAGE_PATH.exists()
+
+
+def test_reading_sdmx_ml_21():
+    assert SDMX_ML_21_MESSAGE_PATH.exists()
+
+
+def test_reading_sdmx_ml_30():
+    assert SDMX_ML_30_MESSAGE_PATH.exists()
