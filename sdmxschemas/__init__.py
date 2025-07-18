94bf24284af66eb6dc1d597c31585e566a222aca
Jose Franco <jose.franco@meaningfuldata.eu>
2025-07-18 13:42:50 +0200

    add sdmx 3.1 schemas

--- sdmxschemas/__init__.py
+++ sdmxschemas/__init__.py
@@ -32,3 +32,7 @@ SDMX_ML_21_MESSAGE_PATH: Path = SDMX_ML_21_BASE_PATH / "SDMXMessage.xsd"
 # SDMX-ML 3.0 schemas
 SDMX_ML_30_BASE_PATH: Path = Path(__file__).parent / "xml" / "sdmx30"
 SDMX_ML_30_MESSAGE_PATH: Path = SDMX_ML_30_BASE_PATH / "SDMXMessage.xsd"
+
+# SDMX-ML 3.1 schemas
+SDMX_ML_31_BASE_PATH: Path = Path(__file__).parent / "xml" / "sdmx31"
+SDMX_ML_31_MESSAGE_PATH: Path = SDMX_ML_31_BASE_PATH / "SDMXMessage.xsd"
